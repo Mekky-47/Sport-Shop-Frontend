a6e1d556afb32a9ebdbec3d89275bd76dccd7709
Abdelrhman Mekky <abdomky685@gmail.com>
2026-04-04 01:20:30 +0200

    Change API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', // Laravel standard local port
+  baseURL: 'https://sportshop.xo.je//api', // Laravel standard local port
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
