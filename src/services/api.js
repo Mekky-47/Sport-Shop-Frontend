3bd2cef0fe3a9fda04b3ddb7b15a6ea5dfacbdd0
Abdelrhman Mekky <abdomky685@gmail.com>
2026-04-04 01:45:51 +0200

    Update API base URL and fetchProducts function
    Updated the base URL for the API to include the products endpoint and modified the fetchProducts function to match.

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sportshop.xo.je', // Laravel standard local port
+  baseURL: 'https://sportshop.xo.je/api/products', // Laravel standard local port
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
 });
 
-export const fetchProducts = () => api.get('/products');
+export const fetchProducts = () => api.get('');
 export const fetchProductById = (id) => api.get(`/products/${id}`);
 export const createProduct = (productData) => api.post('/products', productData);
 export const fetchCategories = () => api.get('/categories');
